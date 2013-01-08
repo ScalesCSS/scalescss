27b16f7196577dedfcd94b5e34bbc38118e53adb
Dave Gillhespy <yodasw16@gmail.com>
2013-01-08 00:06:53 -0500

    base and utilities mostly finished

--- js/plugins.js
+++
@@ -1,8 +0,0 @@
-getTwitters('tweet', { 
-    id: 'devtroit', 
-    count: 3, 
-    enableLinks: true, 
-    ignoreReplies: true, 
-    clearContents: true,
-    template: '"%text%" <a href="http://twitter.com/%user_screen_name%/statuses/%id_str%/">%time%</a>'
-});
--- sass/config.rb
+++ sass/config.rb
@@ -10,10 +10,10 @@ javascripts_dir = "../js"
 fonts_dir = "../type"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+# line_comments = false
